09d382e85273c68417824e54e2c6f5c93739902b
Leon <silenceace@gmail.com>
2026-09-17 10:22:36 +0800

    chore:update

--- weread-bot.py
+++ weread-bot.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 项目信息:
     名称: WeRead Bot
-    版本: 0.3.9
+    版本: 0.3.10
     作者: funnyzak
     仓库: https://github.com/funnyzak/weread-bot
     许可: MIT License
@@ -83,7 +83,7 @@ except ImportError:
     croniter = None
 from zoneinfo import ZoneInfo
 
-VERSION = "0.3.9"
+VERSION = "0.3.10"
 REPO = "https://github.com/funnyzak/weread-bot"
 
 CURRENT_USER = ContextVar("weread_user", default="system")
